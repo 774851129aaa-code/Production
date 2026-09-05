98d2c706eefd784eb56dff9d0aa2eec107dabaa1
774851129aaa-code <774851129aaa@gmail.com>
2026-09-04 17:32:39 -0900

    Update cloud-waf-proxy.ts

--- cloud-waf-proxy.ts
+++ cloud-waf-proxy.ts
@@ -10095,4 +10095,22 @@ if (typeof global !== 'undefined' && !globalInitialized(global)) {
       });
   }
 }
+    .catch((error) => {
+      console.error('[Routix WAF] Failed to start server:', error);
+      process.exit(1);
+    });
+  }
+}
+
+// حماية السيرفر من أخطاء الاستدعاء المكرر للبورت
+if (typeof server !== 'undefined' && server) {
+  const originalListen = server.listen;
+  (server as any).listen = function (...args: any[]) {
+    if (server.listening) {
+      console.log('[Routix WAF] Server is already listening, skipping duplicate call.');
+      return server;
+    }
+    return originalListen.apply(this, args);
+  };
+}
 
